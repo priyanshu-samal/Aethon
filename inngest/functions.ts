2a56cdee45a98f7eba9a042b737e85a29552bc00
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-10-29 13:07:37 +0530

    08-sentry

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -3,6 +3,7 @@ import {createGoogleGenerativeAI} from "@ai-sdk/google";
 import {generateText} from "ai"
 import {createOpenAI} from "@ai-sdk/openai"
 import {createAnthropic} from "@ai-sdk/anthropic"
+import * as Sentry from "@sentry/nextjs";
 
 const google= createGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY || ""
@@ -21,12 +22,20 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend","5s")
     
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+
+
     const {steps:geminiSteps}=await step.ai.wrap(
       "gemini-generate-text",
       generateText,{
         model:google("gemini-2.5-flash"),
       system:"You are a helpful assistant that helps users by generating text based on their prompts.",
       prompt:"what is 2+2?",
+      experimental_telemetry: {
+    isEnabled: true,
+    recordInputs: true,
+    recordOutputs: true,
+  },
       
     }
     )
@@ -37,6 +46,11 @@ export const execute = inngest.createFunction(
         model:openai("gpt-4"),
       system:"You are a helpful assistant that helps users by generating text based on their prompts.",
       prompt:"what is 2+2?",
+      experimental_telemetry: {
+    isEnabled: true,
+    recordInputs: true,
+    recordOutputs: true,
+  },
       
     }
     )
@@ -47,6 +61,11 @@ export const execute = inngest.createFunction(
         model:anthropic("claude-sonnet-4-5"),
       system:"You are a helpful assistant that helps users by generating text based on their prompts.",
       prompt:"what is 2+2?",
+      experimental_telemetry: {
+    isEnabled: true,
+    recordInputs: true,
+    recordOutputs: true,
+  },
       
     }
     )
